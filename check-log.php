8b27a0ce1bed8e8f08a085470f5051d51215e003
xiaochi <cumt.xiaochi@gmail.com>
2019-10-08 03:35:41 +0800

    error_log

--- check-log.php
+++ check-log.php
@@ -7,10 +7,12 @@ $log_file = $argv[1];
 
 $size = filesize($log_file);
 $f = fopen($log_file, "r");
-$offset = max(0, $size - 1000);
+$offset = max(0, $size - 10000);
 $r = fseek($f, $offset);
 
+$cnt=0;
 while ($line = fgets($f)) {
+    $cnt++;
     if (preg_match('#"GET /(ajax/libs/jquery/([\d.]+)/jquery.min.js) HTTP/1.1" 404 \d+#', $line, $m)) {
         $version = $m[2];
         $url = "https://ajax.aspnetcdn.com/ajax/jQuery/jquery-$version.min.js";
@@ -30,3 +32,5 @@ while ($line = fgets($f)) {
 }
 
 fclose($f);
+
+error_log("check $log_file $cnt lines");
